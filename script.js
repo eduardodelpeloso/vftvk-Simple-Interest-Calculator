7a468445c1df609c180d4e8e0778454fca40b0c5
Eduardo Fernandez del Peloso <eduardodelpeloso@gmail.com>
2021-12-07 13:58:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    if (principal <= 0) {
+        alert("Enter a positive number")
+        return;
+    }
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    var result = document.getElementById("result")
+    var result = document.getElementById("result");
 
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 
